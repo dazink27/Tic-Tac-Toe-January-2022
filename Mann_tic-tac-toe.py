f2fc744c3de2bf702e8f915f8f25fcae18907028
dazink27 <brenner.mann27@gmail.com>
2022-01-10 11:18:03 -0700

    started working on a branch

---
+++ Mann_tic-tac-toe.py
@@ -0,0 +1,20 @@
+"""
+# for this assignment, I think I will split this program into three different groups 
+# One thru three, two thru six, and six thru nine.
+# First, I will ask the user to give me a number, then assign that number to a function acording to their input.  
+
+What I need to do is run a for loop through each row.  the for loop will check for variables and replace a variable with X?
+"""
+
+def row_one():
+    print("row one")
+    
+def row_two():
+    print("row two goes here")
+
+def row_three():
+    print("row three here!")
+
+row_one()
+row_two()
+row_three()
