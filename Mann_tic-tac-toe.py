593ccd8c10d5ca1d977557ccb38293fc459126a3
dazink27 <brenner.mann27@gmail.com>
2022-01-10 11:36:57 -0700

    changed branch two to a new method

--- Mann_tic-tac-toe.py
+++ Mann_tic-tac-toe.py
@@ -3,18 +3,25 @@
 # One thru three, two thru six, and six thru nine.
 # First, I will ask the user to give me a number, then assign that number to a function acording to their input.  
 
-What I need to do is run a for loop through each row.  the for loop will check for variables and replace a variable with X?
+The program will print out three rows, 
+and then using a for loop, will replace numbers with letters in the row acording to user input
 """
+player_input=None
+row_one=[1,2,3]
+row_two=[4,5,6]
+row_thr=[7,8,9]
 
-def row_one():
-    print("row one")
-    
-def row_two():
-    print("row two goes here")
+print(row_one)
+print(row_two)
+print(row_thr)
 
-def row_three():
-    print("row three here!")
+player_input=int(input("What Number? "))
+print(f"You picked {player_input}")
+print()
 
-row_one()
-row_two()
-row_three()
+if player_input in range(1,3):
+    print("Your in row one!")
+elif player_input in range(4,6):
+    print("Your in row two!")
+elif player_input in range(7,9):
+    print("Your in row three!")
