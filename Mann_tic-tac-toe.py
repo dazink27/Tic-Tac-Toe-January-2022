892dda1bbbf1d3e82bc7fbfbcf76177b46669311
dazink27 <brenner.mann27@gmail.com>
2022-01-10 19:00:49 -0700

    Meow

--- Mann_tic-tac-toe.py
+++ Mann_tic-tac-toe.py
@@ -6,7 +6,8 @@
 The program will print out three rows, 
 and then using a for loop, will replace numbers with letters in the row acording to user input
 """
-player_input=None
+global victory_achieved
+victory_achieved=False
 row_one=[1,2,3]
 row_two=[4,5,6]
 row_thr=[7,8,9]
@@ -19,22 +20,123 @@ range_1=range(1,4)
 range_2=range(3,7)
 range_3=range(6,10)
 
-def player_one_input():
-    player_input=int(input("What Number? "))
-    print(f"You picked {player_input}")
-    print()
-    return player_input
 
-def row_parser():
+
+
+
+def X_player_turn():
+    player_input=int(input("Number? "))
+    print(f"You picked {player_input}!")
+    print()
     if player_input in range_1:
         print("Your in row one!")
-        print()
+        for slot in row_one:
+            if slot == player_input:
+                # print("Found It!")
+                row_one[(slot)-1]="X"
+                # print(row_one)
+                # print()
+            else:
+                # print("Not Found")
+                # print()
+                pass
+
     elif player_input in range_2:
         print("Your in row two!")
-        print()
+        for slot in row_two:
+            if slot == player_input:
+                # print("Found It!")
+                row_two[(slot)-4]="X"
+                # print(row_two)
+                # print()
+            else:
+                # print("Not Found")
+                # print()
+                pass
     elif player_input in range_3:
         print("Your in row three!")
-        print()
+        for slot in row_thr:
+            if slot == player_input:
+                #print("Found It!")
+                row_thr[(slot)-7]="X"
+                # print(row_thr)
+                #print()
+            else:
+                #print("Not Found")
+                #print()
+                pass
+
+    # elif player_input == None:
+    #     print("No Input!")
     else:
         print("Not Valid")
-row_parser()
+
+def show_table():
+    print(row_one)
+    print(row_two)
+    print(row_thr)
+
+def O_player_turn():
+    player_input=int(input("Number? "))
+    print(f"You picked {player_input}!")
+    print()
+    if player_input in range_1:
+        print("Your in row one!")
+        for slot in row_one:
+            if slot == player_input:
+                # print("Found It!")
+                row_one[(slot)-1]="O"
+                # print(row_one)
+                # print()
+            else:
+                # print("Not Found")
+                # print()
+                pass
+
+    elif player_input in range_2:
+        print("Your in row two!")
+        for slot in row_two:
+            if slot == player_input:
+                # print("Found It!")
+                row_two[(slot)-4]="O"
+                # print(row_two)
+                # print()
+            else:
+                # print("Not Found")
+                # print()
+                pass
+    elif player_input in range_3:
+        print("Your in row three!")
+        for slot in row_thr:
+            if slot == player_input:
+                #print("Found It!")
+                row_thr[(slot)-7]="O"
+                # print(row_thr)
+                #print()
+            else:
+                #print("Not Found")
+                #print()
+                pass
+
+    # elif player_input == None:
+    #     print("No Input!")
+    else:
+        print("Not Valid")
+
+def victory_check():
+    set_row_one=set(row_one)
+    simplified_row=len(set_row_one)
+    print(f"row one contains {simplified_row}")
+    if simplified_row==1:
+        print("Victory Achieved!")
+        victory_achieved=True
+        return victory_achieved
+    else:
+        pass
+
+while victory_achieved != True:
+    X_player_turn()
+    show_table()
+# O_player_turn()
+# show_table()
+    victory_check()
